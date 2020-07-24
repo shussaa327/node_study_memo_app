99436be822ed9712a21bc1cfd01740df052b31a0
shunsuke sasaki <shun.sskd05@icloud.com>
2020-07-24 19:42:32 +0900

    [Add] Database-MYSQL

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
 app.use(express.static('public'));
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: HIDE['MySQL_PASSWORD'],
+  database: ''
+});
 
 app.get('/', (req, res) => {
   res.render('top.ejs');
